983614a897fc6caeb4a02347a6847b9eebd032e3
Doug Sellers <DouglasSSellers@gmail.com>
2022-11-01 20:01:17 -0400

    added carousel feature

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,7 +1,30 @@
-import React from "react";
-
-const Data = () => {
-  return <div>Data</div>;
-};
-
-export default Data;
+export const Data = [
+  {
+    title: "Look at how Hip She looks",
+    text: "This Fall look Hipper than all the Hipsters",
+    image:
+      "https://images.unsplash.com/photo-1602731602206-17b2a3e07990?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzh8fGZhbGwlMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    className: "data-item",
+  },
+  {
+    title: "Boots for every Ocassion",
+    text: "Whether it's for dinner or going for a walk in the woods, boots are a fall must-have",
+    image:
+      "https://images.unsplash.com/photo-1573089214313-443baafcaf63?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTF8fGZhbGwlMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    className: "data-item",
+  },
+  {
+    title: "Fall Hikes",
+    text: "Find attire for that next outdoor adventure",
+    image:
+      "https://images.unsplash.com/photo-1630930196161-03b17bcc77e6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODV8fGZhbGwlMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    className: "data-item",
+  },
+  {
+    title: "Dogs in Fall",
+    text: "Dogs love fall fashion",
+    image:
+      "https://images.unsplash.com/photo-1546975554-31053113e977?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODR8fGZhbGwlMjBmYXNoaW9ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    className: "data-item",
+  },
+];
